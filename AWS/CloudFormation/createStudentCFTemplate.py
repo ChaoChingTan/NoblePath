5c26cef5831aa52c180e103be4b9ece4ee75bfe3
Chao <tcc25@np.edu.sg>
2024-06-30 16:13:36 +0800

    Added USERPREFIX to .env

---
+++ AWS/CloudFormation/createStudentCFTemplate.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from jinja2 import Environment, FileSystemLoader
+
+templateFile = 'iamUserTemplate.j2'
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv('.env'))
+load_dotenv(find_dotenv('.env.secret'))
+
+# Load environment variables
+group_name = os.getenv('GROUP_NAME', 'MyIAMGroup')
+num_users = int(os.getenv('NUM_USERS', 1))
+init_pass = os.getenv('INITPASS')
+##account_id = os.getenv('ACCOUNT_ID')
+
+if not init_pass:
+    raise ValueError("INITPASS environment variable is required")
+
+# Set up Jinja2 environment
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+# Load the template
+template = env.get_template(templateFile)
+
+# Render the template with multiple environment variables
+output = template.render(group_name=group_name, num_users=num_users, init_pass=init_pass)
+
+print(output)
