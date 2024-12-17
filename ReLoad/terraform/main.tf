48ff72f26140638a9353c8f4f855c834da192fd2
Chao <tcc25@np.edu.sg>
2024-12-17 18:01:43 +0800

    Added resources to create an IAM group named grouping, attach the grp_table_readonly policy to it, and create an IAM user under the specified prefix with membership in the group.

--- ReLoad/terraform/main.tf
+++ ReLoad/terraform/main.tf
@@ -85,3 +85,45 @@ resource "aws_dynamodb_table" "stf_table" {
 #    prevent_destroy = true
 #  }
 }
+
+resource "aws_iam_policy" "grp_table_readonly" {
+  name        = "grp_table_readonly_policy"
+  description = "IAM policy for readonly access to grp_table"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ]
+        Effect   = "Allow",
+        Resource = aws_dynamodb_table.grp_table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group" "grouping" {
+  name = "${var.env}grouping"
+  path = "/${var.app}/${var.env}/"
+}
+
+resource "aws_iam_group_policy_attachment" "grouping_policy_attach" {
+  group      = aws_iam_group.grouping.name
+  policy_arn = aws_iam_policy.grp_table_readonly.arn
+}
+
+resource "aws_iam_user" "grouping_user" {
+  name = "${var.env}grouping"
+  path = "/${var.app}/${var.env}/"
+}
+
+resource "aws_iam_group_membership" "grouping_membership" {
+  name  = "grouping_membership"
+  group = aws_iam_group.grouping.name
+  users = [aws_iam_user.grouping_user.name]
+}
